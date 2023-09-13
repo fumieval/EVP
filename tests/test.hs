b57caede753b21eb2da2c05185fb7146ef2de6de
Fumiaki Kinoshita <fumiaki.kinoshita@herp.co.jp>
2023-09-13 18:14:04 +0900

    add more comments to test.hs

--- tests/test.hs
+++ tests/test.hs
@@ -3,6 +3,9 @@
 
 import EVP qualified
 
+-- | List of environment variables defined as a GADT.
+-- The use of GADT ensures that the identifiers correspond to the actual variable names,
+-- and the parsed values are well-typed
 data Var a where
     API_TOKEN :: Var String
     HTTP_PORT :: Var Int
@@ -11,13 +14,18 @@ data Var a where
 deriving instance Show (Var a)
 
 main :: IO ()
-main = EVP.scan $ do
+main = EVP.scan parser
+
+-- ApplicativeDo is important here because Scan is not a monad.
+parser :: EVP.Scan ()
+parser = do
     -- @secret@ disables logging
     _token <- EVP.secret $ EVP.string API_TOKEN
     -- parse the environment variable as a YAML value
     _port <- EVP.yaml HTTP_PORT
     -- obtain the environment variable as is
     _ <- EVP.string FOO
+    -- you can also provide a default value
     _ <- EVP.yamlDefault DEBUG_MODE False
     pure ()
 
